255da5e867039c290ee4c9ae8e14639ba8161488
tesfalemt <tesfalemtekie27@gmail.com>
2022-09-25 17:16:32 -0500

    dashboard

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import axios from "axios";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 import Base from "./component/Base";
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8080/api/";
   return (
+    <>
       <Provider store={store}>
         <div className="App">
-          <Base></Base>
+         
+          <Base>
+          
+          </Base>
         </div>
       </Provider>
+      </>
   );
 }
 
--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -1,7 +1,87 @@
-export default function (){
-    return (
-        <div>
-            Home
-        </div>
-    )
+import { Row, Col, Container, Form, Button, Image } from "react-bootstrap";
+
+import propertyDetails from "../../img";
+
+export default function Home() {
+  return (
+    <Container   style={{margin: "   "}}>
+      <Row>
+        <Col md={{ offset: 4, span: 4 }} xs={{ offset: 4, span: 4 }}>
+          Find it. Tour it. Own it.
+        </Col>
+      </Row>
+
+      <Row  >
+        <Col md={10}>
+          <Container>
+            <Row>
+              {propertyDetails.map((property) => {
+                return (
+                  <Col md={4} style={{ border: "" }}>
+                    <Image fluid src={property.image}></Image>
+                    <p>{property.location}</p>
+                    <p>{property.price}</p>
+                  </Col>
+                );
+              })}
+            </Row>
+          </Container>
+        </Col>
+
+        <Col md={2}>
+        <div class="p-3 mb-2 bg-warning text-dark" >
+          <Form className="position-fixed" style={{margin: 12}}  >
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Row>
+                <Col md={3}>
+                  <Form.Label>Price</Form.Label>
+                </Col>
+                <Col md={9}>
+                  <Form.Control type="number" placeholder="Price" />
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Row>
+                <Col md={3}>
+                  <Form.Label>Date</Form.Label>
+                </Col>
+                <Col md={9}>
+                  <Form.Control type="date" placeholder="Price" />
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Row>
+                <Col md={3}>
+                  <Form.Label>Size</Form.Label>
+                </Col>
+                <Col md={9}>
+                  <Form.Control type="number" placeholder="size" />
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Row>
+                <Col md={3}>
+                  <Form.Label>Location</Form.Label>
+                </Col>
+                <Col md={9}>
+                  <Form.Control type="text" placeholder="" />
+                </Col>
+              </Row>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          </div>
+          <div>
+         
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
