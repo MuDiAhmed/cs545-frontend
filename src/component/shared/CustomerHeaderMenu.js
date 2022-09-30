01884b6071aa6c551478f1dd5c3bfd8c988aaf75
Mohamed Youssef <mohamed.ahmed.abdel.monem@gmail.com>
2022-09-30 18:09:35 -0500

    Customer header menu

--- src/component/shared/CustomerHeaderMenu.js
+++ src/component/shared/CustomerHeaderMenu.js
@@ -3,8 +3,8 @@ import {NavDropdown} from "react-bootstrap";
 export default function ({user, onSignOut}){
     return (
         <NavDropdown title={user.profile.preferred_username} id="nav-dropdown">
-            <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4.2">Favorite List</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4.3">Requests</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item eventKey="4.1" onClick={onSignOut}>Sign out</NavDropdown.Item>
         </NavDropdown>
