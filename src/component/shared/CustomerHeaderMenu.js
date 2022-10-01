40403b2a12a13ed74778f8a82271eedaf52d144d
Mohamed Youssef <mohamed.ahmed.abdel.monem@gmail.com>
2022-10-01 17:52:37 -0500

    reset password flow

--- src/component/shared/CustomerHeaderMenu.js
+++ src/component/shared/CustomerHeaderMenu.js
@@ -1,5 +1,7 @@
 import {NavDropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import ResetPasswordLink from "./ResetPasswordLink";
+import ResetPasswordThroughEmailLink from "./ResetPasswordThroughEmailLink";
 
 export default function ({user, onSignOut}){
     return (
@@ -7,6 +9,8 @@ export default function ({user, onSignOut}){
             <NavDropdown.Item eventKey="4.2"><Link to="/favoriteList" >Favorite List</Link></NavDropdown.Item>
             <NavDropdown.Item eventKey="4.3" ><Link to="/favoriteList" >Requests</Link></NavDropdown.Item>
             <NavDropdown.Divider />
+            <ResetPasswordLink/>
+            <ResetPasswordThroughEmailLink/>
             <NavDropdown.Item eventKey="4.1" onClick={onSignOut}>Sign out</NavDropdown.Item>
         </NavDropdown>
     )
